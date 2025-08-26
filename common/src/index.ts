4ac47a9fd8c1023fb4a55845fcf945d9c2c8f9f2
Gireesh koshe <gireeshkoshe30@gmail.com>
2025-08-26 15:24:21 +0530

    Initial commit (backend)

---
+++ common/src/index.ts
@@ -0,0 +1,29 @@
+import z, { email, string } from "zod"
+
+export const signupInput=z.object({
+    email: z.email(),
+    password:z.string().min(4),
+    name:z.string().optional()
+})
+
+export type SignupInput=z.infer<typeof signupInput>
+
+export const signinInput=z.object({
+    email: z.email(),
+    password:z.string().min(4),
+})
+
+export type SigninInput=z.infer<typeof signinInput>
+
+export const createBlogInput= z.object({
+    title:z.string(),
+    content : z.string()
+})
+export type CreateBlogInput=z.infer <typeof createBlogInput>
+
+export const updateBlogInput= z.object({
+    title:z.string(),
+    content : z.string(),
+    id:z.string()
+})
+export type UpdateInput=z.infer <typeof updateBlogInput>
